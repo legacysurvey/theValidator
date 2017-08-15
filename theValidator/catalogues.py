5cd35ca3c79ebe7fd31ecbb631b7b6e0515c483d
Kaylan Burleigh <kaylanb@berkeley.edu>
2017-08-15 10:49:16 -0700

    modfied legacypipe.format_catalogue to convert dr3 style tractor catalogue to dr4,5 naming scheme

--- theValidator/catalogues.py
+++ theValidator/catalogues.py
@@ -16,6 +16,274 @@ def read_lines(fn_list):
     fin.close()
     return np.sort(np.array( list(np.char.strip(lines)) ))
 
+class ConvertTractor(object):
+    """Convert DR3 fits_table to DR4 and beyond naming scheme
+    """
+    def isOld(self,cols):
+        """cols: T.get_columns()"""
+        if 'decam_flux' in cols:
+            return True
+        else:
+            return False
+   
+    def old_to_new(self,T):
+        """T: fits_table()
+        """
+        #T = fits_table(fn)
+        #hdr = T.get_header()
+        #primhdr = fitsio.read_header(fn)
+        os.environ['DUST_DIR']= '/home/kaylan/myrepo/dust'
+        #outfn=fn.replace('.fits','-newformat.fits')
+        return self.format_catalog(T, 'ugrizY', outfn=None,
+                                   in_flux_prefix="decam_",
+                                   flux_prefix='',write=False)
+    
+    def format_catalog(self, T, allbands, outfn,
+                       in_flux_prefix='', flux_prefix='',write=False):
+        # args: hdr, primhdr
+        
+        # Retrieve the bands in this catalog.
+        bands = allbands
+        print('Bands in this catalog:', bands)
+        
+        has_wise =    'wise_flux'    in T.columns()
+        has_wise_lc = 'wise_lc_flux' in T.columns()
+        has_ap =      'decam_apflux'       in T.columns()
+        # Expand out FLUX and related fields from grz arrays to 'allbands'
+        # (eg, ugrizY) arrays.
+        B = np.array([allbands.index(band) for band in bands])
+        keys = ['flux', 'flux_ivar', 'rchi2', 'fracflux', 'fracmasked', 'fracin',
+                'nobs', 'anymask', 'allmask', 'psfsize', 'depth', 'galdepth']
+        if has_ap:
+            keys.extend(['apflux', 'apflux_resid', 'apflux_ivar'])    
+            
+        for k in keys:
+            incol = '%s%s' % (in_flux_prefix, k)
+            X = T.get(incol)
+            # Handle array columns (eg, apflux)
+            sh = X.shape
+            if len(sh) == 3:
+                nt,nb,N = sh
+                A = np.zeros((len(T), len(allbands), N), X.dtype)
+                A[:,B,:] = X
+            else:
+                A = np.zeros((len(T), len(allbands)), X.dtype)
+                # If there is only one band, these can show up as scalar arrays.
+                if len(sh) == 1:
+                    A[:,B] = X[:,np.newaxis]
+                else:
+                    A[:,B] = X
+            T.delete_column(incol)
+            # FLUX_b for each band, rather than array columns.
+            for i,b in enumerate(allbands):
+                T.set('%s%s_%s' % (flux_prefix, k, b), A[:,i])
+        
+        # WISE
+        wise_prefix='wise_'
+        wise_keys=[]
+        if has_wise:
+            wise_keys.extend(['flux', 'flux_ivar','nobs','rchi2','fracflux'])
+        if has_wise_lc:
+            wise_keys.extend(['lc_flux','lc_flux_ivar','lc_nobs','lc_rchi2',
+                              'lc_fracflux','lc_mjd'])
+        for k in wise_keys:
+            if 'lc_' in k:
+                wisebands=['w1','w2']
+            else:
+                wisebands=['w1','w2','w3','w4']
+            B_wise = np.array([wisebands.index(band) for band in wisebands])
+            incol = '%s%s' % (wise_prefix, k)
+            #raise ValueError
+            X = T.get(incol)
+            # Handle array columns (eg, apflux)
+            sh = X.shape
+            if len(sh) == 3:
+                nt,nb,N = sh
+                A = np.zeros((len(T), len(wisebands), N), X.dtype)
+                A[:,B_wise,:] = X
+            else:
+                A = np.zeros((len(T), len(wisebands)), X.dtype)
+                # If there is only one band, these can show up as scalar arrays.
+                if len(sh) == 1:
+                    A[:,B_wise] = X[:,np.newaxis]
+                else:
+                    A[:,B_wise]= X
+            T.delete_column(incol)
+            # FLUX_b for each band, rather than array columns.
+            for i,b in enumerate(wisebands):
+                T.set('%s%s_%s' % ('', k, b), A[:,i])
+        ### Done WISE
+        
+        
+        from tractor.sfd import SFDMap
+        print('Reading SFD maps...')
+        sfd = SFDMap()
+        filts = ['%s %s' % ('DES', f) for f in allbands]
+        wisebands = ['WISE W1', 'WISE W2', 'WISE W3', 'WISE W4']
+        ebv,ext = sfd.extinction(filts + wisebands, T.ra, T.dec, get_ebv=True)
+        T.ebv = ebv.astype(np.float32)
+        ext = ext.astype(np.float32)
+        decam_ext = ext[:,:len(allbands)]
+        if has_wise:
+            wise_ext  = ext[:,len(allbands):]
+            
+        wbands = ['w1','w2','w3','w4']
+        
+        trans_cols_opt  = []
+        trans_cols_wise = []
+
+        # No MW_TRANSMISSION_* columns at all
+        for i,b in enumerate(allbands):
+            col = 'mw_transmission_%s' % b
+            T.set(col, 10.**(-decam_ext[:,i] / 2.5))
+            trans_cols_opt.append(col)
+            if has_wise:
+                for i,b in enumerate(wbands):
+                    col = 'mw_transmission_%s' % b
+                    T.set(col, 10.**(-wise_ext[:,i] / 2.5))
+                    trans_cols_wise.append(col)
+                    
+        from legacypipe.survey import release_number
+        T.release = np.zeros(len(T), np.int16) + release_number
+        
+        # Column ordering...
+        cols = ['release', 'brickid', 'brickname', 'objid', 'brick_primary', 
+                'type', 'ra', 'dec', 'ra_ivar', 'dec_ivar',
+                'bx', 'by', 'dchisq', 'ebv', 'mjd_min', 'mjd_max']
+        
+        def add_fluxlike(c):
+            for b in allbands:
+                cols.append('%s%s_%s' % (flux_prefix, c, b))
+        def add_wiselike(c, bands=wbands):
+            for b in bands:
+                cols.append('%s_%s' % (c, b))
+        
+        add_fluxlike('flux')
+        if has_wise:
+            add_wiselike('flux')
+            add_fluxlike('flux_ivar')
+        if has_wise:
+            add_wiselike('flux_ivar')
+        if has_ap:
+            for c in ['apflux', 'apflux_resid','apflux_ivar']:
+                add_fluxlike(c)
+                
+        cols.extend(trans_cols_opt)
+        cols.extend(trans_cols_wise)
+        
+        for c in ['nobs', 'rchisq', 'fracflux']:
+            add_fluxlike(c)
+            if has_wise:
+                add_wiselike(c)
+        for c in ['fracmasked', 'fracin', 'anymask', 'allmask']:
+            add_fluxlike(c)
+        if has_wise:
+            pass # Only DR4 on has a wise mask
+        #for i,b in enumerate(wbands[:2]):
+        #    col = 'wisemask_%s' % (b)
+        #    T.set(col, T.wise_mask[:,i])
+        #    cols.append(col)
+        for c in ['psfsize', 'psfdepth', 'galdepth']:
+            add_fluxlike(c)
+        
+        if has_wise:
+            cols.append('wise_coadd_id')
+        if has_wise_lc:
+            for c in ['lc_flux', 'lc_flux_ivar', 'lc_nobs', 'lc_fracflux',
+                      'lc_rchisq','lc_mjd']:
+                add_wiselike(c, bands=['w1','w2'])
+                cols.extend([
+                    'fracdev', 'fracdev_ivar',
+                    'shapeexp_r', 'shapeexp_r_ivar',
+                    'shapeexp_e1', 'shapeexp_e1_ivar',
+                    'shapeexp_e2', 'shapeexp_e2_ivar',
+                    'shapedev_r',  'shapedev_r_ivar',
+                    'shapedev_e1', 'shapedev_e1_ivar',
+                    'shapedev_e2', 'shapedev_e2_ivar',])
+        # match case to T.
+        cc = T.get_columns()
+        cclower = [c.lower() for c in cc]
+        for i,c in enumerate(cols):
+            if (not c in cc) and c in cclower:
+                j = cclower.index(c)
+                cols[i] = cc[j]
+
+        # Units
+        deg = 'deg'
+        degiv = '1/deg^2'
+        arcsec = 'arcsec'
+        flux = 'nanomaggy'
+        fluxiv = '1/nanomaggy^2'
+        units = dict(
+            ra=deg, dec=deg, ra_ivar=degiv, dec_ivar=degiv, ebv='mag',
+            shapeexp_r=arcsec, shapeexp_r_ivar='1/arcsec^2',
+            shapedev_r=arcsec, shapedev_r_ivar='1/arcsec^2')
+        # WISE fields
+        wunits = dict(flux=flux, flux_ivar=fluxiv,
+                      lc_flux=flux, lc_flux_ivar=fluxiv)
+        # Fields that take prefixes (and have bands)
+        funits = dict(
+            flux=flux, flux_ivar=fluxiv,
+            apflux=flux, apflux_ivar=fluxiv, apflux_resid=flux,
+            psfdepth=fluxiv, galdepth=fluxiv, psfsize=arcsec)
+        # add prefixes
+        units.update([('%s%s' % (flux_prefix, k), v) for k,v in funits.items()])
+        # add bands
+        for b in allbands:
+            units.update([('%s%s_%s' % (flux_prefix, k, b), v)
+                          for k,v in funits.items()])
+            # add WISE bands
+        for b in wbands:
+            units.update([('%s_%s' % (k, b), v)
+                          for k,v in wunits.items()])
+            
+        # Create a list of units aligned with 'cols'
+        units = [units.get(c, '') for c in cols]
+        
+        # Cleanup
+        # Catalogue
+        for k_new,k_old in zip(['psfdepth','rchisq'],
+                               ['depth','rchi2']):
+            for b in bands:
+                T.rename('%s_%s' % (k_old,b),'%s_%s' % (k_new,b))
+                #old_col= '%s_%s' % (k_old,b)
+                #if old_col in T.get_columns():
+                #    T.rename(old_col,'%s_%s' % (k_new,b))
+                #else:
+                #    print('warning'.upper() + ' %s not in fits_table' % old_col)
+                    
+        for k_new,k_old in zip(['rchisq'],
+                               ['rchi2']):
+            for b in ['w1','w2','w3','w4']:
+                T.rename('%s_%s' % (k_old,b),'%s_%s' % (k_new,b))
+        for k_new,k_old in zip(['lc_rchisq'],
+                               ['lc_rchi2']):
+            for b in ['w1','w2']:
+                T.rename('%s_%s' % (k_old,b),'%s_%s' % (k_new,b))    
+        for d_key in ['decam_mw_transmission']:
+            if d_key in T.get_columns():
+                T.delete_column(d_key)
+        # ONLY in DR4 on
+        for col in ['mjd_min','mjd_max','wise_coadd_id']:
+            if col in cols:
+                i= cols.index(col)
+                cols.pop(i)
+                units.pop(i)
+        # check that cleanup worked
+        for col in cols:
+            if not col in T.get_columns():
+                raise KeyError('%s in col but not in T' % col)
+            
+        #T.writeto(outfn, columns=cols, header=hdr, primheader=primhdr, units=units)
+        if write:
+            T.writeto(outfn, columns=cols, units=units)
+            print('Wrote %s' % outfn)
+        else:
+            print('Skipping writing of %s' % outfn)
+        return T
+
+
+
 class CatalogueFuncs(object):
     '''funcs for using Dustins fits_table objects'''
     def stack(self,fn_list,textfile=True,
